745453f2d680284d10bbbaf05a1ded2b5b25c805
BrunoTI-Code <155826835+BrunoTI-Code@users.noreply.github.com>
2024-02-14 21:10:11 -0300

    Update ControleEditora.ts

--- src/controle/ControleEditora.ts
+++ src/controle/ControleEditora.ts
@@ -1,7 +1,7 @@
-// ControleEditora.ts
 import { Editora } from '../modelo/Editora';
 
 // Array de objetos Editora que representa as editoras existentes
+
 let editoras: Editora[] = [
   new Editora(1, 'Intrínseca'),
   new Editora(2, 'Globo'),
@@ -9,13 +9,16 @@ let editoras: Editora[] = [
 ];
 
 export class ControleEditora {
-  // Método para obter o nome da editora com base no código da editora
+  
+  //obter o nome da editora com base no código da editora
+  
   getNomeEditora(codEditora: number) {
     const editora = editoras.find(e => e.codEditora === codEditora);
     return editora ? editora.nome : '';
   }
 
-  // Método para obter a lista completa de editoras
+  //obter a lista completa de editoras
+  
   getEditoras() {
     return editoras;
   }
